305867f3b149fa19cfdd69fc1b56ed6c21e62e4f
daksh303 <76605122+daksh303@users.noreply.github.com>
2022-07-11 03:29:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,28 @@
 
 
 import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
-
 import './App.css';
-function App() {
-  if(!localStorage.getItem('username')) 
-   return <LoginForm/>
+
+//const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-
-   
-// make the data static
-    <div className="App">
-   <ChatEngine height="100vh"
-	  userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-
-			
+    <ChatEngine
+      height="100vh"
+      userName='Daksh'
+			userSecret='12345'
 			projectID='cb335cd3-4675-4aa2-bc31-f5119b19fe17'
-    //render our own chat props
-    // editing out the main chat Feed using this syntax check in docs
-
-    // Chat Feed <ChatFeed {...props} /> receives all Chat Engine props, 
-    // and will render a chat feed for the active chat.
-    renderChatFeed = {(chatAppProps)=> <ChatFeed {... chatAppProps} />} />
-    onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    </div>
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
